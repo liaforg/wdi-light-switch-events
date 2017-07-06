c621d8301f597462df1dc35676032d5857030b18
Lia Forgione <liaforg@gmail.com>
2017-07-06 14:38:18 -0400

    light switch completed

--- js/main.js
+++ js/main.js
@@ -1,2 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
 
+  var lightSwitch = document.querySelector('.switch');
+  var status = document.querySelector('.status');
+  var body = document.querySelector('body');
 
+  var off = function() {
+    lightSwitch.className = "switch off";
+    body.className = 'dark';
+    status.innerText = "Hey, who turned off the lights?";
+  };
+
+  var on = function() {
+    lightSwitch.className = "switch on";
+    body.className = 'light';
+    status.innerText = "It's so bright in here!"
+  }
+
+  lightSwitch.addEventListener('click', function(light) {
+    light.preventDefault();
+    light.stopPropagation();
+
+    if(lightSwitch.classList.contains('on'))
+      off();
+    else
+      on();
+
+  });
+
+});
